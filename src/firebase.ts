2b8d1fb67df9327f497f4784ba20f8d933786899
yoon <87020020+Jeongseoyoon@users.noreply.github.com>
2024-09-27 11:26:17 +0000

    update info for my mobile invitation

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,7 +4,13 @@ import { getDatabase } from 'firebase/database';
 // TODO: https://firebase.google.com/docs/web/setup?hl=ko
 // 가이드에 따라 firebase config를 설정합니다.
 const firebaseConfig = {
-
+    apiKey: "AIzaSyAFaThEZso7usxpgOyPxlwO1Z_gwsKyFEY",
+    authDomain: "wedding-54b7a.firebaseapp.com",
+    databaseURL: "https://wedding-54b7a-default-rtdb.firebaseio.com",
+    projectId: "wedding-54b7a",
+    storageBucket: "wedding-54b7a.appspot.com",
+    messagingSenderId: "257361340832",
+    appId: "1:257361340832:web:1488576c8b6de989afdcc5"
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
